e682703f107dd1e8c943c9110156c79916ff74b2
Maxim Matveev <maxx.matveev@gmail.com>
2026-02-06 17:26:34 +0300

    [Feature] Add lint to pomodoro project
    - Добавлена проверка кода через lint как через явные команды, так и через Vite
    - Добавлена проверка качества кода через TSC --noemit

---
+++ src/apps/web/pomodoro_client/eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+    { ignores: ['dist', 'node_modules'] },
+
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['**/*.{ts,tsx,js}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'error',
+        },
+    },
+]
